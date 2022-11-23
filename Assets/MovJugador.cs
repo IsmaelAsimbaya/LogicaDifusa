182d0c148310f3ef0710c508a0763eef6625c190
IsmaelAsimbaya <85204681+IsmaelAsimbaya@users.noreply.github.com>
2022-11-23 12:16:06 -0500

    Initial commit

---
+++ Assets/MovJugador.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovJugador : MonoBehaviour
+{
+
+    public float velocidad = 15;
+    public float velAngular = 60;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, -velAngular * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, velAngular * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, 0, velocidad * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, 0, -velocidad * Time.deltaTime));
+        }
+
+
+
+    }
+}
